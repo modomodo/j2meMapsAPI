ccc615270046dc3ca26bec0439289c89144b1160
Michael Muita <michael.muita.dev@gmail.com>
2015-11-24 10:21:59 +0300

    Updated SQL Table script
    Updated creation of nearstation if not exists and split lat and long
    fields for easier use in code

--- nearstation/nearstation.sql
+++ nearstation/nearstation.sql
@@ -10,6 +10,9 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE DATABASE IF NOT EXISTS nearstation;
+
+USE nearstation;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -122,7 +125,8 @@ CREATE TABLE IF NOT EXISTS `petrostations` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `brandid` int(10) NOT NULL,
   `stationname` varchar(50) NOT NULL,
-  `latlong` varchar(40) NOT NULL,
+  `lat` varchar(40) NOT NULL,
+  `long` varchar(40) NOT NULL,
   `locationid` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
@@ -131,51 +135,51 @@ CREATE TABLE IF NOT EXISTS `petrostations` (
 -- Dumping data for table `petrostations`
 --
 
-INSERT INTO `petrostations` (`id`, `brandid`, `stationname`, `latlong`, `locationid`) VALUES
-(1, 1, 'Ngong Road', '-1.2988654,36.7594331', 30),
-(2, 1, 'Lower Kabete', '-1.2986523,36.6896686', 30),
-(3, 1, 'Opposite Garden City,Thika Road', '-1.2986523,36.6896686', 30),
-(4, 1, 'Argwings Kodhek', '-1.2986523,36.6896686', 30),
-(5, 1, 'Mbagathi', '-1.3061176,36.7509517', 30),
-(6, 1, 'pioneer phase 2', '-1.2855236,36.785284', 30),
-(7, 1, 'Kenya Pipeline', '-1.248879,36.8508996', 30),
-(8, 1, 'Ngara Road', '-1.2776374,36.829705', 30),
-(9, 1, 'General Waruinge Road', '-1.2764693,36.8352561', 30),
-(10, 1, 'Kayole Junction Kangundo Road', '-1.2578579,36.9226657', 30),
-(11, 1, 'Tsavo Road', '-1.3080337,36.8331614', 30),
-(12, 1, 'Woodly Estate', '-1.2986883,36.7809141', 30),
-(13, 1, 'Outering Road', '-1.338497, 36.739769', 30),
-(14, 1, 'Koma Rock ,Nairobi Kenya', '-1.2504852,36.8428578', 30),
-(15, 1, 'DonHolm Road', '-1.300555, 36.888133', 30),
-(16, 1, 'Kangemi', '-1.2544826,36.6788663', 30),
-(17, 1, 'Mkoma Road', '-1.310652, 36.837304', 30),
-(18, 1, 'Waiyaki way', '-1.258676, 36.781317', 30),
-(19, 1, 'Waiyaki way', '-1.259663, 36.778528', 30),
-(20, 1, 'Mbotela', '-1.297978, 36.868376', 30),
-(21, 1, 'Rabai Road', '-1.296616, 36.876920', 30),
-(22, 1, 'Karen C', '-1.339457, 36.740489', 30),
-(23, 1, 'Naivasha road', '-1.263259, 36.720232', 30),
-(24, 1, 'South B', '-1.307991, 36.833302', 30),
-(25, 1, 'Mathare', '-1.266705, 36.844992', 30),
-(26, 2, 'Bernard Estate', '-1.2806332,36.7671369', 30),
-(27, 2, 'Limuru Road', '-1.228599, 36.803137', 30),
-(28, 2, 'Waiyaki way', '-1.259919, 36.785702', 30),
-(29, 2, 'Nairobi West ', '-1.310702, 36.817606', 30),
-(30, 2, 'Ngong Road', '-1.2988973,36.7636815', 30),
-(31, 2, 'Suna Road', '-1.300508, 36.779732', 30),
-(32, 2, 'Argwings Kodhek', '-1.295122, 36.799420', 30),
-(33, 2, 'Madaraka Estate', '-1.310578, 36.817144', 30),
-(34, 2, 'Nairobi West Estate', '-1.310702, 36.817642', 30),
-(35, 2, 'South B', '-1.305561, 36.827268', 30),
-(36, 2, 'Tena Road Outer Ring Road', '-1.293895, 36.886370', 30),
-(37, 2, 'Kuwinda ,Nairobi City', '-1.316619, 36.718413', 30),
-(38, 2, 'Ngara', '-1.272469, 36.831829', 30),
-(39, 2, 'Karen ', '-1.321881, 36.706660', 30),
-(40, 2, 'Westlands', '-1.268975, 36.817296', 30),
-(41, 2, 'City Square', '-1.280708, 36.827893', 30),
-(42, 2, 'City Square', '-1.292429, 36.821789', 30),
-(43, 2, 'Thome', '-1.232526, 36.875087', 30),
-(44, 2, 'Ngong Road', '-1.341366, 36.666834', 30);
+INSERT INTO `petrostations` (`id`, `brandid`, `stationname`, `lat`,`long`, `locationid`) VALUES
+(1, 1, 'Ngong Road', '-1.2988654','36.7594331', 30),
+(2, 1, 'Lower Kabete', '-1.2986523','36.6896686', 30),
+(3, 1, 'Opposite Garden City,Thika Road', '-1.2986523','36.6896686', 30),
+(4, 1, 'Argwings Kodhek', '-1.2986523','36.6896686', 30),
+(5, 1, 'Mbagathi', '-1.3061176','36.7509517', 30),
+(6, 1, 'pioneer phase 2', '-1.2855236','36.785284', 30),
+(7, 1, 'Kenya Pipeline', '-1.248879','36.8508996', 30),
+(8, 1, 'Ngara Road', '-1.2776374','36.829705', 30),
+(9, 1, 'General Waruinge Road', '-1.2764693','36.8352561', 30),
+(10, 1, 'Kayole Junction Kangundo Road', '-1.2578579','36.9226657', 30),
+(11, 1, 'Tsavo Road', '-1.3080337','36.8331614', 30),
+(12, 1, 'Woodly Estate', '-1.2986883','36.7809141', 30),
+(13, 1, 'Outering Road', '-1.338497','36.739769', 30),
+(14, 1, 'Koma Rock ,Nairobi Kenya', '-1.2504852','36.8428578', 30),
+(15, 1, 'DonHolm Road', '-1.300555','36.888133', 30),
+(16, 1, 'Kangemi', '-1.2544826','36.6788663', 30),
+(17, 1, 'Mkoma Road', '-1.310652','36.837304', 30),
+(18, 1, 'Waiyaki way', '-1.258676','36.781317', 30),
+(19, 1, 'Waiyaki way', '-1.259663','36.778528', 30),
+(20, 1, 'Mbotela', '-1.297978','36.868376', 30),
+(21, 1, 'Rabai Road', '-1.296616','36.876920', 30),
+(22, 1, 'Karen C', '-1.339457','36.740489', 30),
+(23, 1, 'Naivasha road', '-1.263259','36.720232', 30),
+(24, 1, 'South B', '-1.307991','36.833302', 30),
+(25, 1, 'Mathare', '-1.266705','36.844992', 30),
+(26, 2, 'Bernard Estate', '-1.2806332','36.7671369', 30),
+(27, 2, 'Limuru Road', '-1.228599','36.803137', 30),
+(28, 2, 'Waiyaki way', '-1.259919','36.785702', 30),
+(29, 2, 'Nairobi West ', '-1.310702','36.817606', 30),
+(30, 2, 'Ngong Road', '-1.2988973','36.7636815', 30),
+(31, 2, 'Suna Road', '-1.300508','36.779732', 30),
+(32, 2, 'Argwings Kodhek', '-1.295122','36.799420', 30),
+(33, 2, 'Madaraka Estate', '-1.310578','36.817144', 30),
+(34, 2, 'Nairobi West Estate', '-1.310702','36.817642', 30),
+(35, 2, 'South B', '-1.305561','36.827268', 30),
+(36, 2, 'Tena Road Outer Ring Road', '-1.293895','36.886370', 30),
+(37, 2, 'Kuwinda ,Nairobi City', '-1.316619','36.718413', 30),
+(38, 2, 'Ngara', '-1.272469','36.831829', 30),
+(39, 2, 'Karen ', '-1.321881','36.706660', 30),
+(40, 2, 'Westlands', '-1.268975','36.817296', 30),
+(41, 2, 'City Square', '-1.280708','36.827893', 30),
+(42, 2, 'City Square', '-1.292429','36.821789', 30),
+(43, 2, 'Thome', '-1.232526','36.875087', 30),
+(44, 2, 'Ngong Road', '-1.341366','36.666834', 30);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
